14c6bb02b4061e02ad887e5bb6fcdc48d2f65895
nereasalazar19 <salazarricharte@gmail.com>
2025-09-16 16:25:45 +0200

    Añadido el campo teléfono al index y a la validación

--- frontend/validacio.js
+++ frontend/validacio.js
@@ -1,14 +1,23 @@
-document.getElementById("formContacte").addEventListener("submit", function(event) {
-  const nom = document.getElementById("nom").value;
-  const email = document.getElementById("email").value;
+document
+  .getElementById("formContacte")
+  .addEventListener("submit", function (event) {
+    const nom = document.getElementById("nom").value;
+    const email = document.getElementById("email").value;
+    const telefon = document.getElementById("telefon").value;
+    const regexTelefon = /^[0-9]{9}$/;
 
-  if (nom.length < 3) {
-    alert("El nom ha de tindre almenys 3 caràcters.");
-    event.preventDefault();
-  }
+    if (nom.length < 3) {
+      alert("El nom ha de tindre almenys 3 caràcters.");
+      event.preventDefault();
+    }
 
-  if (!email.includes("@")) {
-    alert("El correu ha de contindre un '@'.");
-    event.preventDefault();
-  }
-});
+    if (!email.includes("@")) {
+      alert("El correu ha de contindre un '@'.");
+      event.preventDefault();
+    }
+
+    if (!regexTelefon.test(telefon)) {
+      alert("El telèfon ha de tindre exactament 9 dígits numèrics.");
+      event.preventDefault();
+    }
+  });
